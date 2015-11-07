1a9ae86892db3ebe0784ef5a14b6025bceb360e8
Nicolas <nicolas.damay@hotmail.fr>
2015-11-07 19:19:46 +0100

    Visualize some training images

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 __author__ = 'nnico'
 from Data import DataHandler
-
+import pandas as pd
+import matplotlib.pyplot as plt
 
 ########################################################################################################################
 # Interface for the data.
@@ -12,10 +13,30 @@ MyDataHandler = DataHandler()
 if __name__ == "__main__":
 
     print "hello world"
+    ####################################################################################################################
+    # Get data
+    ####################################################################################################################
+    train = MyDataHandler.get_train()
+    test = MyDataHandler.get_test()
 
-    train_data = MyDataHandler.get_train()
-    test_data = MyDataHandler.get_test()
+    ####################################################################################################################
+    # Visualize some training images:
+    ####################################################################################################################
+    print list(train.columns.values)
+    print list(test.columns.values)
 
-    print test_data.hist()
+    MyDataHandler.visualize_train_image(index=0, features=True)
+    MyDataHandler.visualize_train_image(index=1, features=True)
+    MyDataHandler.visualize_train_image(index=2, features=True)
+    # print "Analyse data type: {}\n".format(train.dtypes)
+    # print "Analyse data info: {}".format(train.info())
+    # print "Describe data: {}".format(train.describe())
+
+    ####################################################################################################################
+    # Visualize Train data:
+    ####################################################################################################################
+    # print test.values
+    # imgplot = plt.imshow(test.values)
+    # plt.show()
 
     print "Im done!"
