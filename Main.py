e787c45b635479262e52234e0ff2af25fe243643
Nicolas <nicolas.damay@hotmail.fr>
2015-11-08 15:56:05 +0100

    Mise a jour: Integration dim Handler Modele Handler
    Ambriyon de submission

--- Main.py
+++ Main.py
@@ -1,42 +1,96 @@
 __author__ = 'nnico'
-from Data import DataHandler
+from DataHandler import DataHandler
+from DImHandler import DimHandler
+from ModeleHandler import ModeleHandler
+
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import svm
+from patsy import dmatrices
+
+from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+from sklearn.cross_validation import cross_val_score
 
 ########################################################################################################################
 # Interface for the data.
 ########################################################################################################################
-MyDataHandler = DataHandler()
 
+def myprint(str):
+    """
+    Useless.
 
+    Print a message with standart shape.
+    """
+    print "#######################################\n{}\n#######################################\n".format(str)
 
 if __name__ == "__main__":
 
-    print "hello world"
+
+    myprint("Start computations")
+
     ####################################################################################################################
     # Get data
     ####################################################################################################################
+    myprint("Load data")
+    MyDataHandler = DataHandler()
+    print "Loading train"
     train = MyDataHandler.get_train()
-    test = MyDataHandler.get_test()
+
+    #Pretty things up.
+    column_names = {}
+    for i in xrange(96*96):
+        column_names[i] = 'pixel{}'.format(i)
+
+    ####################################################################################################################
+    # Split pixels:
+    ####################################################################################################################
+    myprint("Extract data")
+    train_x = train.iloc[:800]['Image'].apply(lambda x: pd.Series([int(i) for i in x.split(' ')])).rename(columns=column_names)
+    train_y = train.iloc[:800].ix[:, 0:30].fillna(0).astype(int)
+
+    ####################################################################################################################
+    # Reduce dimension
+    ####################################################################################################################
+    myprint("Reduce dimension")
+    MyDimHandler = DimHandler(model='pca', **{'n_components': 0.80, 'whiten': True})
+
+    train_x = MyDimHandler.fit_transform(train_x)
+    ####################################################################################################################
+    # Train models:
+    ####################################################################################################################
+    myprint("Train models")
+    names = list(train.columns.values)
+    models = []
+    models_name = {}
+    for index in xrange(30):
+        model = ModeleHandler("LogisticRegression")
+        models.append(model)
+        models_name[names[i]] = model
+
+    for index, model in enumerate(models):
+        y = np.ravel(train_y.ix[:,index])
+        model.fit(train_x, y)
+        score = model.score(train_x, y)
+        print "For the model {}, the score is: {}".format(names[i], score)
 
     ####################################################################################################################
     # Visualize some training images:
     ####################################################################################################################
-    print list(train.columns.values)
-    print list(test.columns.values)
-
-    MyDataHandler.visualize_train_image(index=0, features=True)
-    MyDataHandler.visualize_train_image(index=1, features=True)
-    MyDataHandler.visualize_train_image(index=2, features=True)
-    # print "Analyse data type: {}\n".format(train.dtypes)
-    # print "Analyse data info: {}".format(train.info())
-    # print "Describe data: {}".format(train.describe())
+    # print list(train.columns.values)
+    # print list(test.columns.values)
+    # MyDataHandler.visualize_train_image(index=6493, features=True)
 
     ####################################################################################################################
-    # Visualize Train data:
+    #
     ####################################################################################################################
-    # print test.values
-    # imgplot = plt.imshow(test.values)
-    # plt.show()
+    myprint("Predict the score of the test data")
 
-    print "Im done!"
+    print "Loading test"
+
+    test = MyDataHandler.get_test()
+    test_x = test.iloc[:800]['Image'].apply(lambda x: pd.Series([int(i) for i in x.split(' ')])).rename(columns=column_names)
+    test_x = MyDimHandler.transform(test_x)
+
+    myprint("Finished")
